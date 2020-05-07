005911388fbfa8d0bf7f39ea71e2517a4d172cf4
hsp@einfo-tech.com <hsp@einfo-tech.com>
2020-05-07 15:16:08 +0800

    git

---
+++ hsp_sheet_full_screen/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright 2019 EINFO HSP
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    'name': "odoo enterprise sheet full screen by HSP",
+
+    'summary': "odoo enterprise sheet full screen",
+   'license': 'LGPL-3',
+   'description': """
+    odoo enterprise sheet full screen
+    odoo企业版表单全屏显示
+   """,
+    'author': 'HSP',
+    'website': "https://www.garage-kit.com",
+    'images': ['static/description/3.png','static/description/2.png'],
+    'category': 'Tools',
+    'version': '13.0.1.0.0',
+  
+    'depends': [
+        'web_enterprise',
+    ],
+    'data': [
+        'views/webclient_templates.xml',
+    ],
+    # 'demo': [
+    #     'demo/report.xml',
+    # ],
+    'installable': True,
+}
